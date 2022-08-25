8342de706019184714b92b33ac76da19896089fd
Kingsley Godwin <kingsleyog95@gmail.com>
2022-08-25 11:19:52 +0100

    Added the needed screenshots.

---
+++ image-filter-starter-code/src/config/config.ts
@@ -0,0 +1,23 @@
+export const config = {
+  "dev": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "", 
+    "aws_region": "",
+    "aws_profile": "",
+    "aws_media_bucket": ""
+  },
+  "jwt": {
+    "secret": "" 
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": ""
+  }
+}
+
